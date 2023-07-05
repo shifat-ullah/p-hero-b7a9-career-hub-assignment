09f078dc9f08561252d90700a4c826b451cbdcb9
Shifat ullah <shifatullah128@gmail.com>
2023-07-05 23:43:19 +0600

    home and main update

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,19 @@
+/* eslint-disable no-unused-vars */
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import App from './App'
 import './index.css'
 import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
-import Jobs from './components/Jobs.jsx';
-import Blog from './components/component/Blog.jsx';
-import Home from './components/component/Home.jsx';
-import Statistics from './components/Statistics.jsx';
+import Home from './components/Home/Home';
+import Statistics from './components/Statistics/Statistics';
+import AppliedJobs from './components/AppliedJobs/AppliedJobs';
+import Blog from './components/Blog/Blog';
+import FeaturedJobs from './components/FeaturedJobs/FeaturedJobs';
+import JobDetails from './components/JobDetails/JobDetails';
+import Error from './components/Error/Error';
 
 const router = createBrowserRouter([
   {
@@ -19,25 +23,35 @@ const router = createBrowserRouter([
       {
         path: "/",
         element: <Home></Home>,
+        loader: () => fetch('/jobs.json')
       },
       {
-        path: "/Statistics",
+        path: "statistics",
         element: <Statistics></Statistics>
       },
       {
-        path: "/Jobs",
-        element: <Jobs></Jobs>
+        path: "applied-jobs",
+        element: <AppliedJobs></AppliedJobs>
       },
       {
-        path: "/blog",
+        path: "blog",
         element: <Blog></Blog>
       },
-    ],
+      {
+        path: "/job-details/:jobID",
+        element: <JobDetails></JobDetails>,
+        loader: ()=>fetch('/jobs.json')
+      },
+      {
+        path: "*",
+        element: <Error></Error>
+      }
+    ]
   },
 ]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <RouterProvider router={router}/>
   </React.StrictMode>,
 )
